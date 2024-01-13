519446f6a65af0a25bb38ffffcb7bee7feda52c2
Marios Tsaltakis <mariostsaltakis@gmail.com>
2024-01-13 14:31:07 +0200

    update on the htaccess and fixed query in map model

--- php/map.php
+++ php/map.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] === "GET") {
         require_once 'map_view.php'; 
         require_once 'map_control.php';
         require_once 'config_sess.php';
-        $results = user_data($conn); //TODO BGALE APO TO SEND DATA KODIKOUS KAI EPIKINDINA DEDOMENA
+        $results = user_data($conn); 
         send_data($results);
         $conn = null;
         $check = null;
--- php/map_model.php
+++ php/map_model.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 function get_user(object $conn) {
-    $select = "SELECT * FROM person;";
+    $select = "SELECT onoma,epitheto,phonenum,usertype,Latitude,Longitude FROM person;";
     $check = $conn->prepare($select);
     $check->execute();
     $result = $check->fetchAll(PDO::FETCH_ASSOC); 
